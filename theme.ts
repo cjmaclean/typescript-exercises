23905452748813fa58a84708bb84132506169b7b
Caspian Maclean <caspianmaclean@gmail.com>
2021-12-07 12:09:14 +0800

    Make a theme class (substituting strings for colours and form controls)

---
+++ theme.ts
@@ -0,0 +1,29 @@
+// theme class, and also some code to test and call it.
+// using classes, which JavaScript doesn't have but TypeScript does.
+// I made theme class in Java and C# for storing button and window background
+// colour, so I'll do similar for this exercise, but I won't implement
+// the GUI part.
+
+// Compiling and running with:
+// tsc theme.ts
+// node theme.js
+
+class Theme {
+    backgroundColor: string;
+    buttonColor: string;
+
+    applyThemeToBackground(target: string) {
+        console.log("Change colour of " + target + " to " + this.backgroundColor);
+    }
+    applyThemeToButton(target: string) {
+        console.log("Change colour of " + target + " to " + this.buttonColor);
+    }
+}
+
+var blueTheme = new Theme();
+blueTheme.backgroundColor = "cyan";
+blueTheme.buttonColor = "lightBlue";
+
+blueTheme.applyThemeToBackground("window background");
+blueTheme.applyThemeToButton("load button");
+blueTheme.applyThemeToButton("save button");
